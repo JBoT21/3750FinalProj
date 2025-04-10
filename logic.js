bc68911972eed5d5ca4a2d62a53d3d6a230da273
Joseph Laudati <laudatijoseph@gmail.com>
2025-04-10 12:54:57 -0400

    Did some changes to the project files.

--- logic.js
+++ logic.js
@@ -1,6 +1,25 @@
+$(document).ready(function() {
+    console.log("This is our prototype! In case anyone finds this, we are working on a final project for our class.");
+   
+});
+
+
 $("#searchButton").click(function() {
     var searchTerm = $("#searchInput").val();
     alert(searchTerm);
 });
 
-$("#")
+$("#homeButton").click(function() {
+    alert("Home button clicked!");
+});
+
+$("#productButton").click(function() {
+    alert("Product button clicked!");
+});
+
+$("#contactButton").click(function() {
+    alert("Contact button clicked!");
+});
+$("#aboutButton").click(function() {
+    alert("About button clicked!");
+});
