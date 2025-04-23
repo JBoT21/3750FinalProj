5d0b08854247000d1e5326586a2dcfaf97e5c8ea
bunderdown <busimmo@clemson.edu>
2025-04-23 13:36:43 -0400

    Add files via upload

---
+++ products.js
@@ -0,0 +1,21 @@
+
+const driver = document.getElementById("driver");
+const wood = document.getElementById("wood");
+const iron = document.getElementById("iron");
+const putter = document.getElementById("putter");
+
+driver.addEventListener("click", function(){
+    document.location = "drivers.html"
+});
+wood.addEventListener("click", function(){
+    document.location = "woods.html"
+});
+
+iron.addEventListener("click", function(){
+    document.location = "irons.html"
+});
+
+putter.addEventListener("click", function(){
+    document.location = "putters.html"
+});
+
